1e1f82d7bb93cd26137366cc512434ddd8423439
Rafael Bachmann <mediumendian@gmail.com>
2017-06-02 19:56:37 +0200

    add simple Server code

--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -1,6 +1,13 @@
 package server;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import parser.FHEMParser;
+import parser.fhemJson.JsonList2;
 
 /**
  * Created by Rafael on 31.05.17.
@@ -8,9 +15,19 @@ import parser.FHEMParser;
 public class Server {
 
     private static FHEMParser parser = FHEMParser.getInstance();
-    public static void main(String[] args) {
-        String jsonString = "{\"Arg\":\"\", \"totalResultsReturned\":57}";
 
-        parser.parse(jsonString);
+    public static void main(String[] args) {
+
+        try {
+            String jsonString = new String(Files.readAllBytes(Paths.get("temp.json")));
+            System.out.println(jsonString);
+            JsonList2 list = parser.parse(jsonString);
+
+            Gson gson = new Gson();
+            System.out.println(gson.toJson(list));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
