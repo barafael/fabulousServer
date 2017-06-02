0d9e6509c0a596a3483bc412650ff5857d18232d
Johannes Köstler <github@johanneskoestler.de>
2017-06-02 18:31:58 +0200

    add working RMI with reference calls

--- rmi_example/RmiClient.java
+++ rmi_example/RmiClient.java
@@ -1,26 +1,65 @@
-import java.rmi.*;
-import java.rmi.registry.*;
-import java.net.*;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Arrays;
 
-public class RmiClient{
-    static public void main(String args[]){
-        ReceiveMessageInterface rmiServer;
+public class RmiClient {
+    //Sensor mySen = new Sensor(0,new Integer[]{0,0,0,0,0});
+    RemoteSensorInterface myPointer;
+
+    public RmiClient(String serverAddress,String serverPort){
+        RemoteInterface rmiServer;
         Registry registry;
-        String serverAddress="localhost"; //args[0];
-        String serverPort="35444"; //args[1];
-        String text=args[0];  //args[2];
-        System.out.println("sending " + text + " to " +serverAddress + ":" + serverPort);
-        try{
-            registry=LocateRegistry.getRegistry(serverAddress,(new Integer(serverPort)).intValue());
-            rmiServer=(ReceiveMessageInterface)(registry.lookup("rmiServer"));
+        String text = "message";
+
+        try {
+            registry = LocateRegistry.getRegistry(serverAddress, (new Integer(serverPort)).intValue());
+            rmiServer = (RemoteInterface) (registry.lookup("rmiServer"));
             // call the remote method
-            rmiServer.receiveMessage(text);
-        }
-        catch(RemoteException e){
+            System.out.println("sending " + text + " to " + serverAddress + ":" + serverPort);
+            rmiServer.receiveMessage(text); // test ob verbindung steht
+
+
+            System.out.println("listed ojects: "+Arrays.toString(registry.list()));
+
+            //System.out.println("on startup: " + myPointer.printTheInt());
+
+            myPointer = (RemoteSensorInterface) registry.lookup("ServerSensorStub");
+            System.out.println("after lookup: "+ myPointer.printTheInt());
+
+            //mySen = (Sensor) registry.lookup("ServerSensor");
+            //System.out.println("after lookup: " + mySen.printTheInt());
+
+            rmiServer.editSensor(66, new Integer[]{6, 6, 6, 6, 6});
+            System.out.println("after editSensor: " + myPointer.printTheInt());
+
+
+           // mySen = (Sensor) registry.lookup("ServerSensor");
+           // System.out.println("again lookup: " + mySen.printTheInt());
+
+            while (true){
+                try {
+                    Thread.sleep(3000);
+                    System.out.println("in loop: " + myPointer.printTheInt());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        } catch (RemoteException e) {
             e.printStackTrace();
-        }
-        catch(NotBoundException e){
+        } catch (NotBoundException e) {
             System.err.println(e);
         }
+    }
+
+    static public void main(String args[]) {
+
+
+        RmiClient client = new RmiClient("localhost","35444");
+
+
     }
 }
