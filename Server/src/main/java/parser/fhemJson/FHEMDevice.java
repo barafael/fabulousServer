2bf4a35e28a6ab45f835c276908731c53c91b68e
Rafael Bachmann <mediumendian@gmail.com>
2017-06-04 16:44:55 +0200

    add internals getter method for fhemdevice

--- Server/src/main/java/parser/fhemJson/FHEMDevice.java
+++ Server/src/main/java/parser/fhemJson/FHEMDevice.java
@@ -6,10 +6,28 @@ import java.util.HashMap;
  * Created by ra on 02.06.17.
  */
 
-class FHEMDevice {
+/* Don't change attribute names! They are needed by Gson to parse jsonList2.
+   If you want to rename an attribute, annotate them with:
+
+  @SerializedName("oldname")
+
+ */
+
+public class FHEMDevice {
     private String Name;
     private String PossibleSets;
     private String PossibleAttrs;
     private FHEMDeviceInternals Internals;
     private FHEMDeviceReadings Readings;
+
+    public FHEMDeviceInternals getInternals() {
+        /* ASSERTION of invariant:
+           Internals != null
+        */
+        if (Internals == null) {
+            /* Explicit assumption validated! */
+            System.err.println("Internals was 0! We assumed this could never be the case.");
+        }
+        return Internals;
+    }
 }
