8d6eedac4d8282a0732b0d510058ac9e2de47df5
Rafael Bachmann <mediumendian@gmail.com>
2017-06-05 00:22:36 +0200

    add password and login name verification

--- rmi_ssl_clientAuth/src/main/java/LoginRequest.java
+++ rmi_ssl_clientAuth/src/main/java/LoginRequest.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
 
 /**
@@ -18,17 +20,40 @@ class LoginRequest implements Serializable {
         return loginName;
     }
 
-    public void setLoginName(String loginName) {
-        //TODO: validate String
-        this.loginName = loginName;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        //TODO: validate String
-        this.password = password;
+    public boolean setLoginName(@NotNull String loginName) {
+        if (loginName.length() >= 2 && loginName.length() <= 100) {
+            this.loginName = loginName;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setPassword(@NotNull String password) {
+        String pattern = "\\A(?=\\S*?[0-9])(?=\\S*?[a-z])(?=\\S*?[A-Z])(?=\\S*?[@#$%^&+=])\\S{8,}\\z";
+        /* Explanations:
+               https://stackoverflow.com/questions/3802192/regexp-java-for-password-validation
+               https://stackoverflow.com/a/3802238/5226836
+               https://stackoverflow.com/a/32649219/5226836
+        \A                # start-of-string
+        (?=.*[0-9])       # a digit must occur at least once
+        (?=.*[a-z])       # a lower case letter must occur at least once
+        (?=.*[A-Z])       # an upper case letter must occur at least once
+        (?=.*[@#$%^&+=])  # a special character must occur at least once
+        (?=\S+$)          # no whitespace allowed in the entire string
+        .{8,}             # anything, at least eight places though
+        \z                # end-of-string
+        */
+        if (password.matches(pattern)) {
+            this.password = password;
+            return true;
+        }
+        System.err.println("WARNING: Password did not contain necessary characters." +
+                "If you see this warning in production, it is a bug. Please report.");
+        this.password = password; //TODO remove this line eventually
+        return false;
     }
 }
