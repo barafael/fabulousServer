f319ab6acf5d17a5a8c042d793e54f5e89bee9fe
Rafael Bachmann <mediumendian@gmail.com>
2017-06-04 15:55:35 +0200

    add regexp attribute

--- Server/src/main/java/parser/fhemJson/FHEMDeviceInternals.java
+++ Server/src/main/java/parser/fhemJson/FHEMDeviceInternals.java
@@ -4,9 +4,16 @@ package parser.fhemJson;
  * Created by ra on 02.06.17.
  */
 
+/* Don't change attribute names! They are needed by Gson to parse jsonList2.
+   If you want to rename an attribute, annotate them with:
+
+  @SerializedName("oldname")
+
+ */
 class FHEMDeviceInternals {
     private String DEF;
     private String NAME;
     private String STATE;
+    private String REGEXP;
     private String currentlogfile;
 }
