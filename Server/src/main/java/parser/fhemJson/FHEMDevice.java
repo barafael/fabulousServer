af16b127b7eb791872e89e92266c6027e245650a
Rafael Bachmann <mediumendian@gmail.com>
2017-06-05 18:03:02 +0200

    add isSensor and isFileLog methods

--- Server/src/main/java/parser/fhemJson/FHEMDevice.java
+++ Server/src/main/java/parser/fhemJson/FHEMDevice.java
@@ -1,5 +1,7 @@
 package parser.fhemJson;
 
+import java.util.Optional;
+
 /**
  * Created by ra on 02.06.17.
  * This class represents the relevant attributes of the
@@ -24,11 +26,34 @@ public class FHEMDevice {
     public FHEMDeviceInternals getInternals() {
         /* ASSERTION of invariant:
            Internals != null
+           This would mean that the underlying FHEM device had no Internals section!
         */
         if (Internals == null) {
             /* Explicit assumption validated! */
             System.err.println("Internals was 0! We assumed this could never be the case.");
         }
         return Internals;
+    }
+
+    public boolean isSensor() {
+        Optional<String> rooms_opt = Attributes.getRooms();
+        if (rooms_opt.isPresent()) {
+            String rooms = rooms_opt.get();
+            /* allow for some commas and whitespace, but include 'app' */
+            /* A .contains() is not enough because some room might include it ("appartment") */
+            /* IDEA includes a nifty regex checker (just put cursor in regex). */
+            String pattern = "(.*,\\s?)*app(,\\s?.*)*";
+            return rooms.matches(pattern);
+        }
+        return false;
+    }
+
+    public boolean isFileLog() {
+        Optional<String> type_opt = Internals.getType();
+        if (type_opt.isPresent()) {
+            String type = type_opt.get();
+            return type.equals("FileLog");
+        }
+        return false;
     }
 }
