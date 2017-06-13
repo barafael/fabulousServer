c2a1b092fc366b6499b7acbb59e640deac204540
Rafael Bachmann <mediumendian@gmail.com>
2017-06-13 17:59:38 +0200

    add stubby room class

--- Server/src/main/java/fhemModel/sensors/Room.java
+++ Server/src/main/java/fhemModel/sensors/Room.java
@@ -1,4 +1,7 @@
 package fhemModel.sensors;
+
+import fhemUtils.FHEMUtils;
+
 /**
  * This class represents a room which associates a FHEM model.
  * @author Rafael
@@ -11,5 +14,33 @@ public class Room {
     public Room(SVGRoomPlan plan, String name) {
         this.plan = plan;
         this.name = name;
+    }
+
+    public Room(String roomname) {
+        String pathToPlan = FHEMUtils.getFHEMDIR() + "roomplans/" + roomname + ".svg";
+        plan = SVGRoomPlan.loadFile(pathToPlan);
+        name = roomname;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        return name.equals(room.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
