72c77fa322dbb440826ec9e48911dc58f4fb7cd2
Rafael Bachmann <mediumendian@gmail.com>
2017-06-13 12:49:45 +0200

    improve handling of jsonList errors

--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import fhemConnection.FHEMConnection;
 import fhemConnection.FHEMClientModeCon;
 import com.google.gson.GsonBuilder;
+import fhemConnection.FHEMNotFoundException;
 import fhemModel.Model;
 import com.google.gson.Gson;
 import parser.FHEMParser;
@@ -19,19 +20,26 @@ class Server {
 
     public static void main(String[] args) {
         FHEMConnection fhc = new FHEMClientModeCon();
-        String jsonList2 = "";
         try {
-            jsonList2 = fhc.getJsonList2();
+            String jsonList2_str = fhc.getJsonList2();
+            JsonList2 list = parser.parse(jsonList2_str);
+
+            Model model = list.toFHEMModel();
+            /* The other way around - just for testing.
+            *  (Convert objects to json again and print)
+            * */
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            System.out.println(gson.toJson(model));
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (FHEMNotFoundException fnfe) {
+            System.err.println("FHEM might not be running.");
+            System.err.println
+                    ("You also might have to set global variables FHEMPL (path to fhem.pl) and " +
+                            "FHEMPORT (fhem telnet port) in your $HOME/.profile");
+            System.err.println("Otherwise, is your telnet port password protected by FHEM? " +
+                    "Client Mode won't work if this is the case because of FHEM. Use telnet directly with a password.");
+            fnfe.printStackTrace();
         }
-        JsonList2 list = parser.parse(jsonList2);
-
-        /* The other way around - just for testing.
-        *  (Convert objects to json again and print)
-        * */
-        Model model = list.toFHEMModel();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        System.out.println(gson.toJson(model));
     }
 }
