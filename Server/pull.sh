485139df0a5510600b45bf320f54b44347634e6a
Rafael Bachmann <mediumendian@gmail.com>
2017-06-17 18:56:27 +0200

    auto-replace with sed, commented out for now

--- Server/pull.sh
+++ Server/pull.sh
@@ -4,4 +4,5 @@ rsync -r fhem:/opt/fhem/log/* ~/fhemlog/
 ssh -l pi fhem "sudo -u fhem perl /opt/fhem/fhem.pl localhost:7072 \"jsonList2\" > jsonList2.json"
 # location of mesp git dir is assumed currently, change this or maybe fix it with a global variable
 rsync fhem:/home/pi/jsonList2.json ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
-
+#sed -i "s/\/opt\/fhem\/log\//~\/fhemlog\//g" ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
+#sed -i "s/\.\/log\//~\/fhemlog\//g" ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
