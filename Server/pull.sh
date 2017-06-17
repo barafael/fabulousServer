67bd317a8a5bddc443653a7ad47c437c5fabd09b
Rafael Bachmann <mediumendian@gmail.com>
2017-06-17 14:01:12 +0200

    add fhem state config pull script

---
+++ Server/pull.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf ~/fhemlog
+rsync -r fhem:/opt/fhem/log/* ~/fhemlog/
+ssh -l pi fhem "sudo -u fhem perl /opt/fhem/fhem.pl localhost:7072 \"jsonList2\" > jsonList2.json"
+# location of mesp git dir is assumed currently, change this or maybe fix it with a global variable
+rsync fhem:/home/pi/jsonList2.json ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
+
