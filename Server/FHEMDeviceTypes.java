c68596cc006efdb12020d79564a61f97f52c0fbd
Rafael Bachmann <mediumendian@gmail.com>
2017-06-20 01:11:36 +0200

    remove old implementation of fhemparser after manual merge

--- Server/FHEMDeviceTypes.java
+++
@@ -1,3 +0,0 @@
-enum FHEMDeviceTypes {
-    allowed, at, autocreate, CUL, CUL_HM, dummy, eventTypes, FHEMWEB, FileLog, Global, HMinfo, netatmo, notify, SVG, telnet, weblink
-}
--- Server/push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-gradle clean
-gradle fatJar
-rsync ./build/libs/Server-all-0.1.0-SNAPSHOT.jar fhem:/home/pi/Server/Server-all.jar
-
