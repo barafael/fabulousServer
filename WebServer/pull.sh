1b91ac5957f4762604c8a7fc62356ad0ca97366b
Rafael Bachmann <mediumendian@gmail.com>
2017-06-19 15:13:33 +0200

    adjust paths in pull/push

--- WebServer/pull.sh
+++ WebServer/pull.sh
@@ -2,7 +2,6 @@
 rm -rf $FHEMMOCKDIR/fhemlog
 rsync -r fhem:/opt/fhem/log/* $FHEMMOCKDIR/fhemlog/
 ssh -l pi fhem "sudo -u fhem perl /opt/fhem/fhem.pl localhost:7072 \"jsonList2\" > jsonList2.json"
-# location of mesp git dir is assumed currently, change this or maybe fix it with a global variable
 rsync fhem:/home/pi/jsonList2.json $FHEMMOCKDIR/jsonList2.json
 #sed -i "s/\/opt\/fhem\/log\//~\/fhemlog\//g" ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
 #sed -i "s/\.\/log\//~\/fhemlog\//g" ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
---
+++ WebServer/push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+gradle clean
+gradle fatJar
+rsync ./build/libs/WebServer-all-3.4.1.jar fhem:/home/pi/Server/WebServer-all.jar
