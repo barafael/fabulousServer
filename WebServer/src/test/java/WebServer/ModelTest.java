8c292db35843f8d9715032ab6a208d85c69fba58
Rafael Bachmann <mediumendian@gmail.com>
2017-06-20 21:50:23 +0200

    make test actually write to file

--- WebServer/src/test/java/WebServer/ModelTest.java
+++ WebServer/src/test/java/WebServer/ModelTest.java
@@ -4,22 +4,28 @@ import WebServer.FHEMParser.FHEMParser;
 import WebServer.FHEMParser.fhemModel.FHEMModel;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 /**
- * Created by ra on 20.06.17.
+ * @author Rafael
+ * @date 20.06.17.
  */
 public class ModelTest {
     @Test
     public void testModelPrint() {
         Optional<FHEMModel> model = FHEMParser.getInstance().getFHEMModel();
-        try {
-            PrintWriter pw = new PrintWriter("fhemmodel.json");
-            model.ifPresent(pw::println);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        if (model.isPresent()) {
+            Path path = Paths.get("fhemmodel.json");
+            try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+                writer.write(model.get().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
