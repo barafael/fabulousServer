41dfaa6502c96fddf9bd22c4a381e1d8ce31d26f
Johannes Köstler <github@johanneskoestler.de>
2017-06-23 17:36:40 +0200

    add hotfix deliver all data in getModel request

--- WebServer/src/main/java/WebServer/Server.java
+++ WebServer/src/main/java/WebServer/Server.java
@@ -304,7 +304,8 @@ public class Server extends AbstractVerticle {
             if (permissionFuture.succeeded()) {
                 List<String> perm = permissionFuture.result();
                 vertx.executeBlocking(future -> {
-                    Optional<String> answerData_opt = parser.getFHEMModel(perm);
+                    //TODO: remove hotfix
+                    Optional<String> answerData_opt = Optional.of(parser.getFHEMModel().get().toJson());//getFHEMModel(perm);
                     if (!answerData_opt.isPresent()) {
                         future.handle(Future.failedFuture(future.cause()));
                     } else {
