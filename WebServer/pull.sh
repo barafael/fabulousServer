7a7c2d6189ff78d6b3b8ba050d7e8fb4402d365c
Rafael Bachmann <mediumendian@gmail.com>
2017-06-26 16:49:22 +0200

    make bash plugin stop complaining (simple variable usage)

--- WebServer/pull.sh
+++ WebServer/pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rm -rf $FHEMMOCKDIR/fhemlog
-rsync -r fhem:/opt/fhem/log/* $FHEMMOCKDIR/fhemlog/
+rm -rf ${FHEMMOCKDIR}/fhemlog
+rsync -r fhem:/opt/fhem/log/* ${FHEMMOCKDIR}/fhemlog/
 ssh fhem "sudo -u fhem perl /opt/fhem/fhem.pl localhost:7072 \"jsonList2\" > jsonList2.json"
-rsync fhem:/home/pi/jsonList2.json $FHEMMOCKDIR/jsonList2.json
+rsync fhem:/home/pi/jsonList2.json ${FHEMMOCKDIR}/jsonList2.json
 #sed -i "s/\/opt\/fhem\/log\//~\/fhemlog\//g" ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
 #sed -i "s/\.\/log\//~\/fhemlog\//g" ~/Uni/4.Semester/MESP/fabulousServer/Server/jsonList2.json
