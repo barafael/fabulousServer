b5d497a3276998d5ce3f6a8a05d6fcfbdb0c70b6
Rafael Bachmann <mediumendian@gmail.com>
2017-07-19 10:29:37 +0200

    dedup timeserie parsing

--- WebServer/src/main/java/webserver/fhemParser/fhemModel/log/Timeserie.java
+++ WebServer/src/main/java/webserver/fhemParser/fhemModel/log/Timeserie.java
@@ -18,7 +18,6 @@ import java.util.regex.Pattern;
 /**
  * This class represents a chronological, sequential list of samples obtained from a FileLog in FHEM.
  *
- * TODO simplify timeserie parsing, dedup
  * @author Rafael
  */
 //TODO before final: privatize this, only public for testing
@@ -71,111 +70,8 @@ public final class Timeserie {
      * @param samples a list of strings directly from a FileLog
      * @param logType the desired filelog type
      */
-    //TODO: simplify by using range from 0 to now
     Timeserie(List<String> samples, LogType logType) {
-        this.legend = new HashMap<>();
-        switch (logType) {
-            case UNKNOWN:
-            case DISCRETE:
-                /* avoid realloc */
-                xs = new ArrayList<>(samples.size() + 5);
-                ys = new ArrayList<>(samples.size() + 5);
-                double currentKey = 0;
-                for (String entry : samples) {
-                    String[] items = entry.split(" ");
-
-                    LocalDateTime dateTime = LocalDateTime.parse(items[0], FHEM_DATE_FORMATTER);
-                    long epochSecond = dateTime.atZone(ZONE_ID).toEpochSecond();
-                    xs.add(epochSecond);
-
-                    String value;
-                    if (items.length <= 4) {
-                        value = items[items.length - 1];
-                    } else {
-                        value = items[3];
-                    }
-
-                    if (!legend.containsValue(value)) {
-                        legend.put(currentKey, value);
-                        ys.add(currentKey);
-                        currentKey++;
-                    } else {
-                        /* get() ok because legend.containsvalue(value) */
-                        //noinspection ConstantConditions
-                        ys.add(legend.entrySet().stream()
-                                .filter(e -> e.getValue()
-                                        .equals(value)).findFirst().get().getKey());
-                    }
-                }
-
-                if (xs.size() > 1) {
-                    oldestStamp = xs.get(0);
-                    newestStamp = xs.get(xs.size() - 1);
-                }
-
-                legend.put(Collections.max(ys) + 1, "Upper");
-                legend.put(Collections.min(ys) - 1, "Lower");
-
-                break;
-            case REAL:
-            case PERCENT:
-                /* avoid realloc */
-                List<Long> local_xs = new ArrayList<>(samples.size() + 5);
-                List<Double> local_ys = new ArrayList<>(samples.size() + 5);
-                for (String entry : samples) {
-                    String[] items = entry.split(" ");
-
-                    LocalDateTime dateTime = LocalDateTime.parse(items[0], FHEM_DATE_FORMATTER);
-                    long epochSecond = dateTime.atZone(ZONE_ID).toEpochSecond();
-                    local_xs.add(epochSecond);
-
-                    double value = 0.0;
-                    Matcher numberMatch = NUMBER_PATTERN.matcher(items[3]);
-                    if (numberMatch.matches()) {
-                        value = Double.parseDouble(items[3]);
-                    }
-                    local_ys.add(value);
-                }
-
-                if (local_xs.size() <= MAX_SIZE) {
-                    xs = local_xs;
-                    ys = local_ys;
-                } else {
-                    int k = local_xs.size() / MAX_SIZE;
-                    long[] timestampQueue = new long[k];
-                    double[] valueQueue = new double[k];
-
-                    List<Long> newTimestamps = new ArrayList<>();
-                    List<Double> newValues = new ArrayList<>();
-
-                    for (int index = 0; index < local_xs.size(); index++) {
-                        int minIndex = index % k;
-                        timestampQueue[minIndex] = local_xs.get(index);
-                        valueQueue[minIndex] = local_ys.get(index);
-                        if (minIndex == 0) {
-                            newTimestamps.add(Math.round(Arrays.stream(timestampQueue).average().orElse(0.0)));
-                            newValues.add(Arrays.stream(valueQueue).average().orElse(0.0));
-                        }
-                    }
-                    System.out.println("Reduced from " + local_xs.size() + " to "
-                            + newTimestamps.size() + " elements!");
-                    xs = newTimestamps;
-                    ys = newValues;
-                }
-
-                if (xs.size() > 1) {
-                    oldestStamp = xs.get(0);
-                    newestStamp = xs.get(xs.size() - 1);
-                }
-
-                legend.put(Collections.max(ys) + 1, "Upper");
-                legend.put(Collections.min(ys) - 1, "Lower");
-
-                break;
-            default:
-                xs = new ArrayList<>();
-                ys = new ArrayList<>();
-        }
+        this(samples, logType, 0, Instant.now().getEpochSecond());
     }
 
     /**
