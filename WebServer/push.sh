f0e097146823e00eb7af6095be36d2423f47ba84
Rafael Bachmann <mediumendian@gmail.com>
2017-07-29 20:51:16 +0200

    add restart to push.sh
    maybe it worx?

--- WebServer/push.sh
+++ WebServer/push.sh
@@ -2,3 +2,4 @@
 gradle fatJar
 rsync ./build/libs/WebServer-all-3.4.1.jar fhem:/home/pi/Server/webserver-all.jar
 rsync ./rules.json fhem:Server/rules.json
+ssh fhem "sudo systemctl restart innoLabServer.service"
